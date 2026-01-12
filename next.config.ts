0283d23736c91f09ea0e69e24efdc878dba2f8b9
Amit Singh <amiteee14003@gmail.com>
2026-01-12 21:41:18 +0530

    prod bug fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
-/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  images: {
-    domains: ["fakestoreapi.com"],
-  },
 };
 
 export default nextConfig;
